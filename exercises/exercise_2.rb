2c4f16959b8a74d89edf4251ecd5f2d73b8b0954
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-10-30 20:17:55 -0700

    solve exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.second
+@store1.update(name: 'White Rock')
+
+puts "Updated Store 1 Name: #{@store1.name}"
