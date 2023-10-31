abac4750807b3c8a651ed8e7ae42e5633415c496
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-10-30 20:20:15 -0700

    solve exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+  # Rest of your Store model code
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Emily", last_name: "Kim", hourly_rate: 55)
+@store2.employees.create(first_name: "Bob", last_name: "Smith", hourly_rate: 50)
+@store2.employees.create(first_name: "Klark", last_name: "Lo", hourly_rate: 65)
+@store1.employees.create(first_name: "Klark", last_name: "Lo", hourly_rate: 60)
+
+puts "Store 1 has #{@store1.employees.count} employees"
+puts "Store 2 has #{@store2.employees.count} employees"
+puts "Total number of employees at all stores: #{Employee.count}"
